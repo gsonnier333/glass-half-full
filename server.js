0f6b330ca2473877f80085b9a9d859b02d30d94d
Greg Sonnier <gsonnier@comcast.net>
2021-05-29 11:26:42 -0600

    express backend set up for serving files to the user from the built site

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("build"));
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname + "/build/index.html"));
+});
+
+app.listen(PORT, () => {
+	console.log(`App running on port ${PORT}!`);
+});
