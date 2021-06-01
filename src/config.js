4207f845431c718e16a0a82ad678b97ed8d61085
Greg Sonnier <gsonnier@comcast.net>
2021-06-01 11:55:40 -0600

    reconfig for heroku deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
 	appId: process.env.APP_ID,
-	redirectUri: `https://glass-half-full.herokuapp.com:${process.env.PORT}`,
+	redirectUri: `https://glass-half-full.herokuapp.com/`,
 	scopes: ["user.read", "mail.readwrite"],
 };
